188f17a9ad66ae40bbf53417c92ca73a7ddcab30
AzizjonMukhtorov <mukhtorovsino13@gmail.com>
2024-07-25 19:15:08 +0500

    Add models, serializers and Category viewset

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, response
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategotyView(viewsets.ViewSet):
+    
+    """A simple viewset for viewing all categories"""
+    
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.request, many=True) 
+        return response.Response(serializer.data)
