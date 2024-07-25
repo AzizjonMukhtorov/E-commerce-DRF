7b264aac886c5ecb611502fa07b1033da9f57f5f
AzizjonMukhtorov <mukhtorovsino13@gmail.com>
2024-07-25 19:53:46 +0500

    Add urls

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, Brand
+
+
+admin.site.register([Category, Product, Brand])
--- product/views.py
+++ product/views.py
@@ -5,12 +5,13 @@ from .models import Category
 from .serializers import CategorySerializer
 
 
-class CategotyView(viewsets.ViewSet):
+class CategoryViewSet(viewsets.ViewSet):
     
     """A simple viewset for viewing all categories"""
     
     queryset = Category.objects.all()
 
     def list(self, request):
-        serializer = CategorySerializer(self.request, many=True) 
+        serializer = CategorySerializer(self.queryset, many=True) 
         return response.Response(serializer.data)
+    
